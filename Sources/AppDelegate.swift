6cf293193a84318857634a5524c24b68752b0967
austinpower1258 <austinwang115@gmail.com>
2026-08-26 03:58:27 -0700

    fix(plugins): contain peers and index shortcut routing

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -15008,7 +15008,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCent
                event: event,
                actions: [],
                shortcuts: configuredCmuxShortcutActions.compactMap(\.shortcut)
-                    + configuredPluginShortcutBindings()
+                    + configuredPluginShortcutBindings(for: event)
            ) {
             return true
         }
